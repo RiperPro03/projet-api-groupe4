fdfcc08e435fe4a472271edc0c4744e385b538f9
Christopher <102330198+RiperPro03@users.noreply.github.com>
2026-06-12 23:14:47 +0200

    fix: env

--- services/follow-service/prisma.config.ts
+++ services/follow-service/prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "prisma/config";
 
 const databaseUrl =
   process.env.DATABASE_URL ??
-  "postgresql://auth_user:auth_password@localhost:5433/auth_db?schema=public";
+  "postgresql://follow_user:follow_password@follow-postgres:5432/follow_db?schema=public";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
